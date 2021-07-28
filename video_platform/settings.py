c1391d6294d589c90df5157be61d40ffa75d0571
ArtusU <artusrock@hotmail.com>
2021-07-28 10:26:24 +0100

    core app

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -37,7 +39,9 @@ ROOT_URLCONF = 'video_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,6 +105,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static_root'
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
