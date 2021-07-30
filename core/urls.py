34fcff0a506b13ecbb9c8dee6f45e2ef4be8c653
ArtusU <artusrock@hotmail.com>
2021-07-30 12:06:25 +0100

    ProfileCreate

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ app_name ='core'
 urlpatterns = [
     path('', Home.as_view()),
     path('profile/',ProfileList.as_view(),name='profile_list'),
+    path('profile/create',ProfileCreate.as_view(),name='profile_create'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
-
 from django.views import View
 
+from .forms import *
 
 class Home(View):
     def get(self, request, *args, **kwargs):
@@ -18,3 +18,21 @@ class ProfileList(View):
         profiles=request.user.profiles.all()
         print(profiles)
         return render(request, 'profileList.html', {'profiles':profiles})
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+    def get(self,request,*args, **kwargs):
+        form=ProfileForm()
+        return render(request, 'profileCreate.html', {'form':form})
+
+    def post(self,request,*args, **kwargs):
+        form=ProfileForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect(f'/watch/{profile.uuid}')
+
+        return render(request, 'profileCreate.html', {'form':form})
