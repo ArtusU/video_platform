b5a0adbf9fdcd4704d37ceddff479d0ab5d5f7b8
ArtusU <artusrock@hotmail.com>
2021-08-02 10:18:10 +0100

    ShowMovieDetail view

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('profile/',ProfileList.as_view(),name='profile_list'),
     path('profile/create', ProfileCreate.as_view(),name='profile_create'),
     path('watch/<str:profile_id>/', Watch.as_view(),name='watch'),
+    path('movie/detail/<str:movie_id>/',ShowMovieDetail.as_view(),name='show_det'),
 ]
