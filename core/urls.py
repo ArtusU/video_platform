04744501988d47cb18c1a6c76783560b58c22221
ArtusU <artusrock@hotmail.com>
2021-08-02 11:00:32 +0100

    ShowMovie view

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('profile/create', ProfileCreate.as_view(),name='profile_create'),
     path('watch/<str:profile_id>/', Watch.as_view(),name='watch'),
     path('movie/detail/<str:movie_id>/',ShowMovieDetail.as_view(),name='show_det'),
+    path('movie/play/<str:movie_id>/',ShowMovie.as_view(),name='play'),
 ]
